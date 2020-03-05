48425264959894e5f8a114c9c4f11d6dc130f8bb
gustavo2892 <gustavo2892@gmail.com>
2020-03-05 14:57:32 -0300

    Install Reactotron

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
       { extensions: ['.jsx', '.js'] }
     ],
     'import/prefer-default-export': 'off',
-    'react/state-in-constructor': [2, 'never']
+    'react/state-in-constructor': [2, 'never'],
+    'no-console': ["error", { allow: ["tron"] }]
   },
 };
